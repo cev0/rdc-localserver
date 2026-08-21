55426011f7cc3c9f5d6bdc1a296e64172734dd1b
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-22 01:41:30 +0400

    WorldV2 müqavilə uyğunluq testini vahid runner-ə qoş

--- dovlet_xerite_worldv2_butun_testler.js
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -12,6 +12,7 @@ async function run() {
   require('./dovlet_xerite_worldv2_lod_qaydalari_testi');
   require('./dovlet_xerite_worldv2_prezident_adapteri_testi');
   require('./dovlet_xerite_worldv2_runtime_metadata_testi');
+  require('./dovlet_xerite_worldv2_muqavile_testi');
 
   await require('./dovlet_xerite_worldv2_handler_testi');
 
