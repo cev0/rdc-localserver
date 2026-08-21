ee02864a24315e194d3f2313a3abc0c5bc163f68
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-21 22:07:03 +0400

    WorldV2 xəritə testlərini vahid runner altında birləşdir

---
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('=== WorldV2 Dövlət xəritəsi testləri başlayır ===\n');
+
+require('./dovlet_xerite_worldv2_qaydalari_testi');
+require('./dovlet_xerite_worldv2_payload_testi');
+require('./dovlet_xerite_worldv2_lifecycle_adapteri_testi');
+
+console.log('\n=== WorldV2 Dövlət xəritəsi bütün testləri tamamlandı ===');
