6bd584697bc31e3bdd5f32af98027a4d76e9bc8d
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-21 22:12:46 +0400

    Qlobal WorldV2 payload testlərini vahid runnerə qoş

--- dovlet_xerite_worldv2_butun_testler.js
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -5,5 +5,6 @@ console.log('=== WorldV2 Dövlət xəritəsi testləri başlayır ===\n');
 require('./dovlet_xerite_worldv2_qaydalari_testi');
 require('./dovlet_xerite_worldv2_payload_testi');
 require('./dovlet_xerite_worldv2_lifecycle_adapteri_testi');
+require('./dovlet_xerite_worldv2_qlobal_payload_testi');
 
 console.log('\n=== WorldV2 Dövlət xəritəsi bütün testləri tamamlandı ===');
