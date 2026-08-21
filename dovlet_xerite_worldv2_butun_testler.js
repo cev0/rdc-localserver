662231b7b3c78af510aabac5ad4ab0b8e7f2fe2c
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-21 22:15:53 +0400

    WorldV2 sərhəd xidməti testlərini vahid runnerə qoş

--- dovlet_xerite_worldv2_butun_testler.js
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -7,5 +7,6 @@ require('./dovlet_xerite_worldv2_payload_testi');
 require('./dovlet_xerite_worldv2_lifecycle_adapteri_testi');
 require('./dovlet_xerite_worldv2_qlobal_payload_testi');
 require('./dovlet_xerite_worldv2_topologiya_testi');
+require('./dovlet_xerite_worldv2_serhed_xidmeti_testi');
 
 console.log('\n=== WorldV2 Dövlət xəritəsi bütün testləri tamamlandı ===');
