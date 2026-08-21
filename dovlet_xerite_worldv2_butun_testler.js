d1819b1e808447902352bf9bcd904952f1bf59aa
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-22 01:37:21 +0400

    WorldV2 vahid test runner-də async handler testini gözlə

--- dovlet_xerite_worldv2_butun_testler.js
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -1,14 +1,24 @@
 'use strict';
 
-console.log('=== WorldV2 Dövlət xəritəsi testləri başlayır ===\n');
+async function run() {
+  console.log('=== WorldV2 Dövlət xəritəsi testləri başlayır ===\n');
 
-require('./dovlet_xerite_worldv2_qaydalari_testi');
-require('./dovlet_xerite_worldv2_payload_testi');
-require('./dovlet_xerite_worldv2_lifecycle_adapteri_testi');
-require('./dovlet_xerite_worldv2_qlobal_payload_testi');
-require('./dovlet_xerite_worldv2_topologiya_testi');
-require('./dovlet_xerite_worldv2_serhed_xidmeti_testi');
-require('./dovlet_xerite_worldv2_lod_qaydalari_testi');
-require('./dovlet_xerite_worldv2_prezident_adapteri_testi');
+  require('./dovlet_xerite_worldv2_qaydalari_testi');
+  require('./dovlet_xerite_worldv2_payload_testi');
+  require('./dovlet_xerite_worldv2_lifecycle_adapteri_testi');
+  require('./dovlet_xerite_worldv2_qlobal_payload_testi');
+  require('./dovlet_xerite_worldv2_topologiya_testi');
+  require('./dovlet_xerite_worldv2_serhed_xidmeti_testi');
+  require('./dovlet_xerite_worldv2_lod_qaydalari_testi');
+  require('./dovlet_xerite_worldv2_prezident_adapteri_testi');
 
-console.log('\n=== WorldV2 Dövlət xəritəsi bütün testləri tamamlandı ===');
+  await require('./dovlet_xerite_worldv2_handler_testi');
+
+  console.log('\n=== WorldV2 Dövlət xəritəsi bütün testləri tamamlandı ===');
+}
+
+run().catch((xeta) => {
+  console.error('\nWorldV2 test runner uğursuz oldu.');
+  console.error(xeta);
+  process.exitCode = 1;
+});
