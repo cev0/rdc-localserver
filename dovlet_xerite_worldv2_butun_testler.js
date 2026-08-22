3f78219bfdf40edef63438c0dc94629720c28b67
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-22 12:46:47 +0400

    WorldV2 production handler testini ümumi runner-a əlavə et

--- dovlet_xerite_worldv2_butun_testler.js
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -17,10 +17,10 @@ async function run() {
   require('./dovlet_xerite_worldv2_muqavile_testi');
   require('./dovlet_xerite_worldv2_izolyasiya_testi');
   require('./dovlet_xerite_worldv2_hazirliq_guard_testi');
-  require('./dovlet_xerite_worldv2_server_launcher_testi');
 
   await require('./dovlet_xerite_worldv2_handler_testi');
   await require('./dovlet_xerite_worldv2_obyekt_handler_testi');
+  await require('./dovlet_xerite_worldv2_obyekt_production_handler_testi');
   await require('./dovlet_xerite_worldv2_invalid_direction_testi');
 
   console.log('\n=== WorldV2 Dövlət xəritəsi bütün testləri tamamlandı ===');
