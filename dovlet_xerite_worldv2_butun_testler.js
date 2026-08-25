900c1b989e599c28d66c4816c718486377eca3ec
Cavidan <34436166+cev0@users.noreply.github.com>
2026-08-25 04:48:16 +0400

    WorldV2 viewed state handler testini ümumi runnerə qoş

--- dovlet_xerite_worldv2_butun_testler.js
+++ dovlet_xerite_worldv2_butun_testler.js
@@ -28,6 +28,7 @@ async function run() {
   await require('./dovlet_xerite_worldv2_info_production_handler_testi');
   await require('./dovlet_xerite_worldv2_obyekt_production_handler_testi');
   await require('./dovlet_xerite_worldv2_qlobal_production_handler_testi');
+  await require('./dovlet_xerite_worldv2_baxis_production_handler_testi');
   await require('./dovlet_xerite_worldv2_elfecin_handler_testi');
   await require('./dovlet_xerite_worldv2_invalid_direction_testi');
 
