8b1a37848b96905f2ae55c370ea07945408c37bf
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-16 14:55:00 +0400

    Route WorldV2 resource operations to SQL authoritative runtime

--- dovlet_xerite_worldv2_resurs_emeliyyat_sistemi.js
+++ dovlet_xerite_worldv2_resurs_emeliyyat_sistemi.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const legacy = require('./dovlet_xerite_worldv2_resurs_emeliyyat_legacy');
+const sqlNative = require('./dovlet_xerite_worldv2_resurs_sql_native');
 const {
   worldV2ResursShadowSyncAktivdir,
   worldV2ResursShadowSyncEtClient,
 } = require('./dovlet_xerite_worldv2_resurs_shadow_sync');
 
+async function sqlIstifadeEtClient(client, stateId) {
+  return await sqlNative.worldV2SqlNativeIstifadeEtClient(client, stateId);
+}
+
 async function shadowSyncEt(client, stateId, targetIds, netice) {
   if (!worldV2ResursShadowSyncAktivdir()) return netice;
   if (!netice || netice.deyisdi !== true) return netice;
-
   const sync = await worldV2ResursShadowSyncEtClient(client, stateId, targetIds);
   if (netice && typeof netice === 'object') {
     Object.defineProperty(netice, 'sqlShadowSync', {
@@ -21,22 +25,62 @@ async function shadowSyncEt(client, stateId, targetIds, netice) {
   return netice;
 }
 
+async function runtimeEmeliyyatiClient(client, stateId, emeliyyat) {
+  if (await sqlIstifadeEtClient(client, stateId)) {
+    throw new Error('SQL authoritative rejimdə generic legacy WorldV2 runtime mutasiyası qadağandır.');
+  }
+  return await legacy.runtimeEmeliyyatiClient(client, stateId, emeliyyat);
+}
+
+async function worldV2ResursHedefiniAlClient(client, stateId, targetId, nowMs = Date.now()) {
+  if (await sqlIstifadeEtClient(client, stateId)) {
+    return await sqlNative.worldV2ResursHedefiniSqlAlClient(client, stateId, targetId, nowMs, false);
+  }
+  return await legacy.worldV2ResursHedefiniAlClient(client, stateId, targetId, nowMs);
+}
+
+async function worldV2ResursHedefiniAl(stateId, targetId, nowMs = Date.now()) {
+  if (await sqlNative.worldV2SqlNativeIstifadeEt(stateId)) {
+    return await sqlNative.worldV2ResursHedefiniSqlAl(stateId, targetId, nowMs);
+  }
+  return await legacy.worldV2ResursHedefiniAl(stateId, targetId, nowMs);
+}
+
+async function worldV2ResursDaxilOlmaVeziyyetiniAlClient(client, melumat) {
+  if (await sqlIstifadeEtClient(client, melumat && melumat.stateId)) {
+    return await sqlNative.worldV2ResursDaxilOlmaVeziyyetiniSqlAlClient(client, melumat);
+  }
+  return await legacy.worldV2ResursDaxilOlmaVeziyyetiniAlClient(client, melumat);
+}
+
 async function worldV2ResursuRezervEtClient(client, melumat) {
+  if (await sqlIstifadeEtClient(client, melumat && melumat.stateId)) {
+    return await sqlNative.worldV2ResursuSqlRezervEtClient(client, melumat);
+  }
   const netice = await legacy.worldV2ResursuRezervEtClient(client, melumat);
   return shadowSyncEt(client, melumat && melumat.stateId, [melumat && melumat.targetId], netice);
 }
 
 async function worldV2ResursSahibliyiniKocurClient(client, melumat) {
+  if (await sqlIstifadeEtClient(client, melumat && melumat.stateId)) {
+    return await sqlNative.worldV2ResursSahibliyiniSqlKocurClient(client, melumat);
+  }
   const netice = await legacy.worldV2ResursSahibliyiniKocurClient(client, melumat);
   return shadowSyncEt(client, melumat && melumat.stateId, [melumat && melumat.targetId], netice);
 }
 
 async function worldV2ResursRezerviniBuraxClient(client, melumat) {
+  if (await sqlIstifadeEtClient(client, melumat && melumat.stateId)) {
+    return await sqlNative.worldV2ResursSqlRezerviniBuraxClient(client, melumat);
+  }
   const netice = await legacy.worldV2ResursRezerviniBuraxClient(client, melumat);
   return shadowSyncEt(client, melumat && melumat.stateId, [melumat && melumat.targetId], netice);
 }
 
 async function worldV2TeleportSaheResurslariniSilClient(client, melumat) {
+  if (await sqlIstifadeEtClient(client, melumat && melumat.stateId)) {
+    return await sqlNative.worldV2TeleportSaheResurslariniSqlSilClient(client, melumat);
+  }
   const netice = await legacy.worldV2TeleportSaheResurslariniSilClient(client, melumat);
   const hedefler = netice && Array.isArray(netice.removedResourceTargetIds)
     ? netice.removedResourceTargetIds
@@ -45,6 +89,9 @@ async function worldV2TeleportSaheResurslariniSilClient(client, melumat) {
 }
 
 async function worldV2ResursToplamaniBitirClient(client, melumat) {
+  if (await sqlIstifadeEtClient(client, melumat && melumat.stateId)) {
+    return await sqlNative.worldV2ResursToplamaniSqlBitirClient(client, melumat);
+  }
   const netice = await legacy.worldV2ResursToplamaniBitirClient(client, melumat);
   return shadowSyncEt(client, melumat && melumat.stateId, [melumat && melumat.targetId], netice);
 }
@@ -53,11 +100,11 @@ module.exports = {
   WORLDV2_RESURS_TARGET_REGEX: legacy.WORLDV2_RESURS_TARGET_REGEX,
   worldV2ResursTargetIdDirmi: legacy.worldV2ResursTargetIdDirmi,
   worldV2ResursTargetiniParcala: legacy.worldV2ResursTargetiniParcala,
-  runtimeEmeliyyatiClient: legacy.runtimeEmeliyyatiClient,
+  runtimeEmeliyyatiClient,
   effektivMesgulluq: legacy.effektivMesgulluq,
-  worldV2ResursHedefiniAlClient: legacy.worldV2ResursHedefiniAlClient,
-  worldV2ResursHedefiniAl: legacy.worldV2ResursHedefiniAl,
-  worldV2ResursDaxilOlmaVeziyyetiniAlClient: legacy.worldV2ResursDaxilOlmaVeziyyetiniAlClient,
+  worldV2ResursHedefiniAlClient,
+  worldV2ResursHedefiniAl,
+  worldV2ResursDaxilOlmaVeziyyetiniAlClient,
   worldV2ResursuRezervEtClient,
   worldV2ResursSahibliyiniKocurClient,
   worldV2ResursRezerviniBuraxClient,
