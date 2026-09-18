94d039c9357c42b01c202947876e72617ced143a
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-18 19:09:10 +0400

    Make convoy technology research PostgreSQL-authoritative

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -42,6 +42,7 @@ const UNIT_TESTLERI = [
   "konvoy_pg_mutasiya_testi.js",
   "konvoy_pvp_info_qayda_override_testi.js",
   "konvoy_texnologiya_inkisaf_inteqrasiya_testi.js",
+  "konvoy_texnologiya_pg_mutasiya_testi.js",
   "konvoy_tutum_formulu_testi.js",
   "konvoy_tutum_texnologiya_disabled_testi.js",
   "konvoy_unity_contract_testi.js",
