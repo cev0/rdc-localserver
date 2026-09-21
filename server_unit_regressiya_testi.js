a28c40141eb2cc7c1d2b723e1cda026ddfbff16f
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 11:48:21 +0400

    Include Last Shelter world battlefield regression

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -62,6 +62,7 @@ const UNIT_TESTLERI = [
   "last_shelter_science_prerequisite_runtime_testi.js",
   "last_shelter_science_tree_reference_testi.js",
   "last_shelter_troop_transfer_reference_testi.js",
+  "last_shelter_world_battlefield_reference_testi.js",
   "missiya_mukafat_pg_mutasiya_testi.js",
   "missiya_snapshot_authority_testi.js",
   "missiya_smoke_testi.js",
