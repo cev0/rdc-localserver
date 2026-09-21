ca5d2a95dea7ddacd8f7f05cc152904c9542ee71
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 18:28:14 +0400

    Add Last Shelter command registry collision test

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -95,6 +95,7 @@ const UNIT_TESTLERI = [
   "runtime_last_shelter_economy_reference_commands_testi.js",
   "runtime_last_shelter_queue_science_commands_testi.js",
   "runtime_last_shelter_troop_reference_commands_testi.js",
+  "runtime_last_shelter_command_registry_testi.js",
   "runtime_last_shelter_init_command_testi.js",
   "missiya_mukafat_pg_mutasiya_testi.js",
   "missiya_snapshot_authority_testi.js",
