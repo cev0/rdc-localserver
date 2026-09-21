52c78898a79a5bc81f631015dd661d055209456e
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 20:03:15 +0400

    Cover full science topology route in registry

--- runtime_last_shelter_command_registry_testi.js
+++ runtime_last_shelter_command_registry_testi.js
@@ -1,30 +1,14 @@
 "use strict";
 
 const assert=require("assert");
-const {
-  lastShelterItemCommandleriniQeydEt
-}=require("./runtime_last_shelter_item_commands");
-const {
-  lastShelterTutorialCommandiniQeydEt
-}=require("./runtime_last_shelter_tutorial_command");
-const {
-  lastShelterWorldCupCommandleriniQeydEt
-}=require("./runtime_last_shelter_worldcup_commands");
-const {
-  lastShelterResourceCommandiniQeydEt
-}=require("./runtime_last_shelter_resource_command");
-const {
-  lastShelterEconomyReferenceCommandleriniQeydEt
-}=require("./runtime_last_shelter_economy_reference_commands");
-const {
-  lastShelterQueueScienceCommandleriniQeydEt
-}=require("./runtime_last_shelter_queue_science_commands");
-const {
-  lastShelterTroopReferenceCommandleriniQeydEt
-}=require("./runtime_last_shelter_troop_reference_commands");
-const {
-  lastShelterInitCommandiniQeydEt
-}=require("./runtime_last_shelter_init_command");
+const {lastShelterItemCommandleriniQeydEt}=require("./runtime_last_shelter_item_commands");
+const {lastShelterTutorialCommandiniQeydEt}=require("./runtime_last_shelter_tutorial_command");
+const {lastShelterWorldCupCommandleriniQeydEt}=require("./runtime_last_shelter_worldcup_commands");
+const {lastShelterResourceCommandiniQeydEt}=require("./runtime_last_shelter_resource_command");
+const {lastShelterEconomyReferenceCommandleriniQeydEt}=require("./runtime_last_shelter_economy_reference_commands");
+const {lastShelterQueueScienceCommandleriniQeydEt}=require("./runtime_last_shelter_queue_science_commands");
+const {lastShelterTroopReferenceCommandleriniQeydEt}=require("./runtime_last_shelter_troop_reference_commands");
+const {lastShelterInitCommandiniQeydEt}=require("./runtime_last_shelter_init_command");
 
 class StrictRouter {
   constructor(){this.routes=new Map();}
@@ -41,7 +25,6 @@ class StrictRouter {
 const state={};
 const getOrCreatePlayerState=()=>state;
 const router=new StrictRouter();
-
 lastShelterItemCommandleriniQeydEt(router,{getOrCreatePlayerState});
 lastShelterTutorialCommandiniQeydEt(router,{getOrCreatePlayerState});
 lastShelterWorldCupCommandleriniQeydEt(router);
@@ -49,49 +32,17 @@ lastShelterResourceCommandiniQeydEt(router,{getOrCreatePlayerState});
 lastShelterEconomyReferenceCommandleriniQeydEt(router,{getOrCreatePlayerState});
 lastShelterQueueScienceCommandleriniQeydEt(router,{getOrCreatePlayerState});
 lastShelterTroopReferenceCommandleriniQeydEt(router);
-lastShelterInitCommandiniQeydEt(router,{
-  getOrCreatePlayerState,
-  ensureFreshPlayerState:async()=>state,
-  updateServerTime:()=>{}
-});
+lastShelterInitCommandiniQeydEt(router,{getOrCreatePlayerState,ensureFreshPlayerState:async()=>state,updateServerTime:()=>{}});
 
 const expected=[
-  "item.buy",
-  "settutorial",
-  "worldcup.list",
-  "worldcup.get",
-  "synuserresource",
-  "activity.list",
-  "activity.get",
-  "shop.list",
-  "shop.get",
-  "repay.info",
-  "alliance.group_purchase.info",
-  "alliance.group_purchase.offer",
-  "vipstore.panel",
-  "queue.list",
-  "science.catalog",
-  "science.topology",
-  "science.prerequisite",
-  "science.plan",
-  "troop.catalog",
-  "troop.get",
-  "troop.transfer.level6",
-  "troop.transfer.type",
-  "troop.transfer.point",
-  "last_shelter.init"
+  "item.buy","settutorial","worldcup.list","worldcup.get","synuserresource",
+  "activity.list","activity.get","shop.list","shop.get","repay.info",
+  "alliance.group_purchase.info","alliance.group_purchase.offer","vipstore.panel",
+  "queue.list","science.catalog","science.topology.list","science.topology",
+  "science.prerequisite","science.plan","troop.catalog","troop.get",
+  "troop.transfer.level6","troop.transfer.type","troop.transfer.point","last_shelter.init"
 ];
 
-assert.deepStrictEqual(
-  [...router.routes.keys()].sort(),
-  expected.sort()
-);
-
-assert.strictEqual(
-  new Set(router.routes.keys()).size,
-  router.routes.size
-);
-
-console.log(
-  "PASS: all Last Shelter runtime command modules register together without route collisions."
-);
+assert.deepStrictEqual([...router.routes.keys()].sort(),expected.sort());
+assert.strictEqual(new Set(router.routes.keys()).size,router.routes.size);
+console.log("PASS: all Last Shelter runtime command modules register together without route collisions.");
