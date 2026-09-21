a646960773d42d190a04f13a185b007b74efa8c3
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 12:09:38 +0400

    Include Last Shelter seven-day activity regression

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -64,6 +64,7 @@ const UNIT_TESTLERI = [
   "last_shelter_science_full_topology_testi.js",
   "last_shelter_science_prerequisite_runtime_testi.js",
   "last_shelter_science_tree_reference_testi.js",
+  "last_shelter_seven_days_reference_testi.js",
   "last_shelter_starter_city_reference_testi.js",
   "last_shelter_task_reference_testi.js",
   "last_shelter_troop_transfer_reference_testi.js",
