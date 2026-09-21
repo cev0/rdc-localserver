ceb33a378676989ef61483115bb57e2106e81d80
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 12:07:47 +0400

    Include Last Shelter auxiliary runtime regression

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -53,6 +53,7 @@ const UNIT_TESTLERI = [
   "last_shelter_building_kataloqu_testi.js",
   "last_shelter_engagement_reward_reference_testi.js",
   "last_shelter_alliance_runtime_contract_testi.js",
+  "last_shelter_auxiliary_runtime_reference_testi.js",
   "last_shelter_item_buy_contract_testi.js",
   "last_shelter_hero_reference_testi.js",
   "last_shelter_fort_troop_reference_testi.js",
