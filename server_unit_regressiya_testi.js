2135cb059040d3f78c0896be9fa85a05b839c378
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 18:04:42 +0400

    Add SynUserResource command test to unit regression

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -73,6 +73,7 @@ const UNIT_TESTLERI = [
   "last_shelter_repay_reference_testi.js",
   "last_shelter_resurs_kataloqu_testi.js",
   "last_shelter_resource_runtime_testi.js",
+  "runtime_last_shelter_resource_command_testi.js",
   "last_shelter_science_kataloqu_testi.js",
   "last_shelter_science_full_topology_testi.js",
   "last_shelter_science_prerequisite_runtime_testi.js",
