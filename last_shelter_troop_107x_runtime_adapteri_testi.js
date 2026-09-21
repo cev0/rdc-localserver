d4281a1b776594f228cd25cbe47620b8f59245d0
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 12:31:16 +0400

    Cover full verified 107x runtime troop families

--- last_shelter_troop_107x_runtime_adapteri_testi.js
+++ last_shelter_troop_107x_runtime_adapteri_testi.js
@@ -10,7 +10,7 @@ const {
 } = require("./last_shelter_troop_107x_runtime_adapteri");
 
 const ids = verified107xIds();
-assert.deepStrictEqual(ids, ["107000", "107001", "107002", "107003", "107004", "107005", "107006", "107007", "107019", "107119", "107219"]);
+assert.deepStrictEqual(ids, ["107000","107001","107002","107003","107004","107005","107006","107007","107008","107009","107100","107101","107102","107103","107104","107105","107106","107107","107108","107109","107200","107201","107202","107203","107204","107205","107206","107207","107208","107209","107019","107119","107219"]);
 
 for (const id of ids) {
   const source = TROOP_107X[id];
@@ -55,6 +55,20 @@ assert.strictEqual(overlaid.stats.hp, 15);
 assert.strictEqual(overlaid.stats.consumption.amount, TROOP_107X["107007"].upkeep);
 assert.deepStrictEqual(Object.fromEntries(overlaid.costPerUnit.map(x => [x.type, x.amount])), { food: 203, wood: 108, stone: 4, iron: 25 });
 assert.strictEqual(overlaid.lastShelterVerified, true);
+
+const vehicleT10 = applyVerified107xToRdcUnit({ unitId: "vehicle_t10", lastShelterArmyId: "107109", stats: {} });
+assert.strictEqual(vehicleT10.baseTrainingSeconds, 173);
+assert.strictEqual(vehicleT10.stats.attack, 90);
+assert.strictEqual(vehicleT10.stats.defense, 65);
+assert.strictEqual(vehicleT10.stats.hp, 21);
+assert.strictEqual(vehicleT10.stats.marchSpeed, 16.100000381469727);
+assert.deepStrictEqual(Object.fromEntries(vehicleT10.costPerUnit.map(x => [x.type, x.amount])), { food: 189, stone: 11, iron: 52 });
+
+const shooterT10 = applyVerified107xToRdcUnit({ unitId: "shooter_t10", lastShelterArmyId: "107209", stats: {} });
+assert.strictEqual(shooterT10.baseTrainingSeconds, 173);
+assert.strictEqual(shooterT10.stats.attack, 65);
+assert.strictEqual(shooterT10.stats.range, 50);
+assert.deepStrictEqual(Object.fromEntries(shooterT10.costPerUnit.map(x => [x.type, x.amount])), { food: 155, wood: 77, stone: 19, iron: 13 });
 
 const advanced = applyVerified107xToRdcUnit({ unitId: "advanced_ranged", lastShelterArmyId: "107219", stats: {} });
 assert.strictEqual(advanced.baseTrainingSeconds, 173);
