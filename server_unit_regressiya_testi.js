d8f18b859dcd1d6473bfa7b356348c2ef9218588
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 16:09:37 +0400

    Add Last Shelter economy runtime regression test

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -91,6 +91,7 @@ const UNIT_TESTLERI = [
   "last_shelter_world_battlefield_reference_testi.js",
   "last_shelter_worldcup_reference_testi.js",
   "runtime_last_shelter_worldcup_commands_testi.js",
+  "runtime_last_shelter_economy_reference_commands_testi.js",
   "missiya_mukafat_pg_mutasiya_testi.js",
   "missiya_snapshot_authority_testi.js",
   "missiya_smoke_testi.js",
