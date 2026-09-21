96a417925785953921e5347cf13b67dc4e66dd93
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 13:35:03 +0400

    Include Last Shelter World Cup regression

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -83,6 +83,7 @@ const UNIT_TESTLERI = [
   "last_shelter_troop_transfer_progression_reference_testi.js",
   "last_shelter_truck_convoy_reference_testi.js",
   "last_shelter_world_battlefield_reference_testi.js",
+  "last_shelter_worldcup_reference_testi.js",
   "missiya_mukafat_pg_mutasiya_testi.js",
   "missiya_snapshot_authority_testi.js",
   "missiya_smoke_testi.js",
