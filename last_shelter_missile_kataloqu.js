2a2fce6eff958a2527c1adb8e6e6d474e280c50e
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 13:02:34 +0400

    Expand verified Last Shelter missile catalog to 53308

--- last_shelter_missile_kataloqu.js
+++ last_shelter_missile_kataloqu.js
@@ -13,7 +13,9 @@ const MISSILE = Object.freeze({
   "53303": Object.freeze({ wood: 500000, iron: 500000, def_value: 4500, speed: 5, item_need: "200047;500", unlock_level: 4, atk_value: 5, time: 54000, capacity: 2 }),
   "53304": Object.freeze({ iron: 50000, def_value: 4500, speed: 4, item_need: "200047;1000", unlock_level: 5, atk_value: 4, time: 86400, capacity: 2, missile_effect: 53501 }),
   "53305": Object.freeze({ wood: 500000, iron: 100000, def_value: 2000, speed: 2, item_need: "200047;525", unlock_level: 3, atk_value: 10, time: 28800, capacity: 1 }),
-  "53306": Object.freeze({ iron: 200000, def_value: 6000, speed: 4.5, item_need: "200047;1200", unlock_level: 6, atk_value: 5, time: 86400, capacity: 2, missile_effect: 53502 })
+  "53306": Object.freeze({ iron: 200000, def_value: 6000, speed: 4.5, item_need: "200047;1200", unlock_level: 6, atk_value: 5, time: 86400, capacity: 2, missile_effect: 53502 }),
+  "53307": Object.freeze({ wood: 1500000, iron: 500000, status: 500905, def_value: 5000, speed: 1.5, item_need: "200047;1500", unlock_level: 7, atk_value: 10, time: 144000, capacity: 2, missile_effect: 53503 }),
+  "53308": Object.freeze({ wood: 500000, def_value: 5000, speed: 1.5, item_need: "200047;200", unlock_level: 8, atk_value: 1, time: 86400, capacity: 1 })
 });
 
 function missileAl(id) {
