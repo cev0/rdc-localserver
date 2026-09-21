6e8ef55d3471f65bde5e70ad9a906ec72471c963
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 11:46:27 +0400

    Include Last Shelter fort troop regression

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -53,6 +53,7 @@ const UNIT_TESTLERI = [
   "last_shelter_building_kataloqu_testi.js",
   "last_shelter_item_buy_contract_testi.js",
   "last_shelter_hero_reference_testi.js",
+  "last_shelter_fort_troop_reference_testi.js",
   "last_shelter_queue_kataloqu_testi.js",
   "last_shelter_resurs_kataloqu_testi.js",
   "last_shelter_resource_runtime_testi.js",
