a27046405f8cafb81aab78e7c9c3739108aee940
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-21 10:53:04 +0400

    Include Last Shelter science tree topology in regression suite

--- server_unit_regressiya_testi.js
+++ server_unit_regressiya_testi.js
@@ -55,6 +55,7 @@ const UNIT_TESTLERI = [
   "last_shelter_queue_kataloqu_testi.js",
   "last_shelter_resurs_kataloqu_testi.js",
   "last_shelter_science_kataloqu_testi.js",
+  "last_shelter_science_tree_reference_testi.js",
   "missiya_mukafat_pg_mutasiya_testi.js",
   "missiya_snapshot_authority_testi.js",
   "missiya_smoke_testi.js",
