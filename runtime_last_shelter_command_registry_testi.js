13c6d0c735834447c9f814dace3cbdc6054a5fd6
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-22 09:47:36 +0400

    Register hero general list in route collision contract

--- runtime_last_shelter_command_registry_testi.js
+++ runtime_last_shelter_command_registry_testi.js
@@ -47,7 +47,7 @@ const expected=[
   "engagement.info","mission.info","building.reference.list","building.reference.get",
   "queue.list","science.catalog","science.topology.list","science.topology","science.state",
   "science.prerequisite","science.plan","troop.catalog","troop.get",
-  "troop.transfer.level6","troop.transfer.type","troop.transfer.point","hero.info","hero.get","hero.general.get","last_shelter.init"
+  "troop.transfer.level6","troop.transfer.type","troop.transfer.point","hero.info","hero.get","hero.general.list","hero.general.get","last_shelter.init"
 ];
 
 assert.deepStrictEqual([...router.routes.keys()].sort(),expected.sort());
