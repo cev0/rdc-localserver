e140d4fc06c3bda068eb960030a318d5c95b64b3
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-22 16:23:49 +0400

    Stop loading legacy mission rule patch

--- server_missiya_genisletme.js
+++ server_missiya_genisletme.js
@@ -1,5 +1,4 @@
 "use strict";
-require("./oyun_qayda_duzelisleri");
 require("./yol_baglanti_qoruyucusu");
 require("./konvoy_recall_handler_override");
 require("./pvp_raport_oyuncu_adi_override");
