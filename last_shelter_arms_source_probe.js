ccea0b797bea37e5db7475c6967ccc0e4bdcd8a3
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-22 18:48:35 +0400

    Probe troop unlock science nodes

--- last_shelter_arms_source_probe.js
+++ last_shelter_arms_source_probe.js
@@ -4,6 +4,14 @@ const {
   sourceCatalog
 } = require("./last_shelter_source_catalog");
 
+const {
+  scienceTopologyMelumatiniAl
+} = require("./last_shelter_science_full_topology");
+
+const {
+  scienceMelumatiniAl
+} = require("./last_shelter_science_kataloqu");
+
 const ids = [
   "107000","107008","107009",
   "107100","107108","107109",
@@ -26,3 +34,24 @@ for (const id of ids) {
 }
 
 sourceCatalog.release("arms");
+
+for (const scienceId of [
+  "973400","973700",
+  "971400","971700",
+  "975400","975700"
+]) {
+  console.log(
+    "[SCIENCE_UNLOCK_PROBE]",
+    scienceId,
+    JSON.stringify({
+      topology:
+        scienceTopologyMelumatiniAl(
+          scienceId
+        ),
+      catalog:
+        scienceMelumatiniAl(
+          scienceId
+        )
+    })
+  );
+}
