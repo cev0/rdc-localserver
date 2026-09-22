18fe78560f566b3c150769665770031c96926154
Cavidan <34436166+cev0@users.noreply.github.com>
2026-09-22 17:48:35 +0400

    Remove synthetic convoy research balance

--- konvoy_qaydalari.js
+++ konvoy_qaydalari.js
@@ -23,58 +23,7 @@ const KONVOY_TEXNOLOGIYA_ACARLARI = Object.freeze({
   IKINCI_KONVOY: "ikinci_konvoy"
 });
 
-const KONVOY_TEXNOLOGIYA_BALANSI = Object.freeze({
-  IKINCI_QEHRAMAN_YERI: Object.freeze({
-    techId: KONVOY_TEXNOLOGIYA_ACARLARI.IKINCI_QEHRAMAN_YERI,
-    displayName: "Konvoy Qəhrəman Yeri II",
-    requiredHqLevel: 3,
-    requiredInstituteLevel: 2,
-    researchTimeSeconds: 10 * 60,
-    requiredTechIds: Object.freeze([]),
-    cost: Object.freeze([
-      Object.freeze({ type: "wood", amount: 500 }),
-      Object.freeze({ type: "iron", amount: 200 }),
-      Object.freeze({ type: "money", amount: 400 })
-    ])
-  }),
-
-  IKINCI_KONVOY: Object.freeze({
-    techId: KONVOY_TEXNOLOGIYA_ACARLARI.IKINCI_KONVOY,
-    displayName: "İkinci Konvoy",
-    requiredHqLevel: 5,
-    requiredInstituteLevel: 4,
-    researchTimeSeconds: 30 * 60,
-    requiredTechIds: Object.freeze([
-      KONVOY_TEXNOLOGIYA_ACARLARI.IKINCI_QEHRAMAN_YERI
-    ]),
-    cost: Object.freeze([
-      Object.freeze({ type: "wood", amount: 1500 }),
-      Object.freeze({ type: "iron", amount: 700 }),
-      Object.freeze({ type: "fuel", amount: 250 }),
-      Object.freeze({ type: "money", amount: 1200 })
-    ])
-  }),
-
-  UCUNCU_QEHRAMAN_YERI: Object.freeze({
-    techId: KONVOY_TEXNOLOGIYA_ACARLARI.UCUNCU_QEHRAMAN_YERI,
-    displayName: "Konvoy Qəhrəman Yeri III",
-    requiredHqLevel: 6,
-    requiredInstituteLevel: 6,
-    researchTimeSeconds: 60 * 60,
-    requiredTechIds: Object.freeze([
-      KONVOY_TEXNOLOGIYA_ACARLARI.IKINCI_KONVOY
-    ]),
-    cost: Object.freeze([
-      Object.freeze({ type: "wood", amount: 3000 }),
-      Object.freeze({ type: "iron", amount: 1400 }),
-      Object.freeze({ type: "fuel", amount: 600 }),
-      Object.freeze({ type: "money", amount: 2500 })
-    ])
-  })
-});
-
 module.exports = {
   KONVOY_QAYDALARI,
-  KONVOY_TEXNOLOGIYA_ACARLARI,
-  KONVOY_TEXNOLOGIYA_BALANSI
+  KONVOY_TEXNOLOGIYA_ACARLARI
 };
